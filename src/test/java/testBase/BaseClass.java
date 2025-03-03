ee4a8a63859597e13bfdef047558a9e75bb08e9e
Govind <govindvaidya8@gmail.com>
2025-03-03 12:23:05 +0530

    Added browser and platform Settings

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -2,6 +2,7 @@ package testBase;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
@@ -10,11 +11,14 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -27,38 +31,67 @@ public class BaseClass {
 	public Logger logger;
 
 	@BeforeClass(groups = { "sanity", "regression", "master" })
-	@Parameters("browser")
-	public void setup(String browser) throws IOException {
+	@Parameters({ "os", "browser" })
+	public void setup(String os, String browser) throws IOException {
 
 		System.setProperty("log4j2.debug", "true");
 		System.setProperty("log4j.configurationFile", "src/test/resources/log4j2.xml");
 
 		String url = ConfigReader.getProperty("url");
-
+		String huburl = ConfigReader.getProperty("huburl");
 		logger = LogManager.getLogger(this.getClass());
 
-		switch (browser.toLowerCase()) {
-		case "chrome":
-			driver = new ChromeDriver();
-			break;
-		case "edge":
-			driver = new EdgeDriver();
-			break;
-		case "firefox":
-			driver = new FirefoxDriver();
-			break;
-		default:
-			System.out.println("Invalid Browser...");
-			// logger.error("Invalid Browser");
-			Assert.fail();
-			return;
+		if (ConfigReader.getProperty("execution_env").equalsIgnoreCase("remote")) {
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+
+			if (os.equalsIgnoreCase("windows")) {
+				capabilities.setPlatform(Platform.WIN10);
+			} else if (os.equalsIgnoreCase("Linux")) {
+				capabilities.setPlatform(Platform.LINUX);
+			} else {
+				System.out.println("No Matching OS....");
+				return;
+			}
+
+			if (browser.equalsIgnoreCase("chrome")) {
+				capabilities.setBrowserName("chrome");
+
+			} else if (browser.equalsIgnoreCase("edge")) {
+				capabilities.setBrowserName("MicrosoftEdge");
+			} else {
+				System.out.println("No Matching Browser...");
+				return;
+			}
+
+			driver = new RemoteWebDriver(new URL(huburl),capabilities);
 		}
+
+		if (ConfigReader.getProperty("execution_env").equalsIgnoreCase("local")) {
+			switch (browser.toLowerCase()) {
+			case "chrome":
+				driver = new ChromeDriver();
+				break;
+			case "edge":
+				driver = new EdgeDriver();
+				break;
+			case "firefox":
+				driver = new FirefoxDriver();
+				break;
+			default:
+				System.out.println("Invalid Browser...");
+				// logger.error("Invalid Browser");
+				Assert.fail();
+				return;
+			}
+		}
+
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().window().maximize();
 		driver.get(url);
 
 	}
+
 
 	@AfterClass(groups = { "sanity", "regression", "master" })
 	public void tearDown() {
